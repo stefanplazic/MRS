a2f6e0f4b90320f4479a5bd165e450f5701528ad
stefanplazic <stefan.plazi@gmail.com>
2020-05-05 09:01:35 +0200

    Patient route update profile data. issue#3

--- routes/patients.js
+++ routes/patients.js
@@ -1,4 +1,3 @@
-
 var express = require('express');
 var router = express.Router();
 var JWT = require('../utils/jwt');
@@ -8,12 +7,13 @@ var PatientData = require('../models').PatientData;
 var Location = require('../models').Location;
 
 /*GET USER PROFILE DATA INFO*/
-router.get('/profileDate', JWT.authMiddleware, async function (req, res, next) {
+router.get('/profileDate', JWT.authMiddleware, JWT.patientMiddleware, async function (req, res, next) {
     try {
+        const userId = req.user.userId;
         //load data from database
-        let user = await User.findOne({ where: { id: req.user.userId } });
-        let location = await Location.findOne({ where: { id: req.user.userId } });
-        let patientdata = await PatientData.findOne({ where: { user_id: req.user.userId } });
+        let user = await User.findOne({ where: { id: userId } });
+        let location = await Location.findOne({ where: { id: user.dataValues.location } });
+        let patientdata = await PatientData.findOne({ where: { user_id: userId } });
 
         //check if some data is null
         if (user == null) return res.json({ status: false, message: 'Internal error' });
@@ -30,8 +30,35 @@ router.get('/profileDate', JWT.authMiddleware, async function (req, res, next) {
         userData.user = { email: user.email, fName: user.fName, lName: user.lName };
         userData.location = { address: location.address, city: location.city, state: location.state, lat: location.lat, lng: location.lng };
         userData.patientdata = { phone: patientdata.phone, identification_number: patientdata.identification_number };
-     
+
         res.json({ success: true, userData: userData });
+    }
+    catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+/*UPDATE USER PROFILE DATA*/
+router.put('/updateProfile', JWT.authMiddleware, JWT.patientMiddleware, async function (req, res, next) {
+    try {
+        const userId = req.user.userId;
+        //load data from database
+        let user = await User.findOne({ where: { id: userId } });
+        let location = await Location.findOne({ where: { id: user.dataValues.location } });
+        let patientdata = await PatientData.findOne({ where: { user_id: userId } });
+
+        //check if some data is null
+        if (user == null) return res.json({ status: false, message: 'Internal error' });
+        if (location == null) return res.json({ status: false, message: 'Internal error' });
+        if (location == null) return res.json({ status: false, message: 'Internal error' });
+
+        //update
+        await User.update({ password: req.body.user.password, fName: req.body.user.fName, lName: req.body.user.lName }, { where: { id: userId } });
+        await Location.update(req.body.location, { where: { id: user.dataValues.location } });
+        await PatientData.update({ phone: req.body.patientdata.phone }, { where: { user_id: userId } });
+
+        res.json({ success: true, message: 'Updated profile successfully' });
     }
     catch (error) {
         console.error(error);
